4f7d26ddecd346dcbea8447d69b963686e52e27c
Max <74901101+datadrivenmax@users.noreply.github.com>
2023-01-12 10:09:36 -0800

    status

--- Assessments/Axoni-Assessment.py
+++ Assessments/Axoni-Assessment.py
@@ -1,7 +1,20 @@
 from collections import defaultdict
 
+# declare test variable
+flights = """
+NY -> Iceland -> London -> Berlin
+NY -> Maine -> London
+Berlin -> Paris -> Amsterdam
+Paris -> London -> Egypt
+"""
+
+for flight in flights.splitlines():
+    cities = flight.split(" -> ")
+print(cities)
+
 def find_shortest_path(flights, origin, desination):
-    pass
 
-
-graph = defaultdict(list)
+    # create graph to represent the flights
+    graph = defaultdict(list)
+    for flight in flights.splitlines():
+        cities = flight.split(" ")
